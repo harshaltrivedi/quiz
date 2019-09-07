9eb3d517e7be9e8d658e5f99e39809a0ab45cbe4
Harshal Trivedi <harshaltrivedi16@gmail.com>
2019-09-07 10:57:10 +1000

    Updates

---
+++ precache-manifest.4f02595a247da579961c1791a317bd95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c569a66c4d92b3cda82402b2d36176c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1b64342d618c441ccc30",
+    "url": "/react-gh-pages/static/css/main.66cfb6c3.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-gh-pages/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1b64342d618c441ccc30",
+    "url": "/react-gh-pages/static/js/main.76852ea1.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
